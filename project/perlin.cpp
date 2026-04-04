b21e4848efb06ab0f3d4bd61f50c347b8ee8eecc
Caramajau <90894366+Caramajau@users.noreply.github.com>
2026-04-04 11:09:51 +0200

    Small improvements to the perlin script

--- project/perlin.cpp
+++ project/perlin.cpp
@@ -7,8 +7,8 @@
 // Sample Perlin noise at coordinates x, y
 float perlin(float x, float y) {
 	// Determine grid cell corner coordinates
-	int x0 = (int)x;
-	int y0 = (int)y;
+	auto x0 = (int)x;
+	auto y0 = (int)y;
 	int x1 = x0 + 1;
 	int y1 = y0 + 1;
 
@@ -53,7 +53,7 @@ float dotGridGradient(int integerX, int integerY, float x, float y) {
     // Compute the distance vector
     float dx = x - (float)integerX;
     float dy = y - (float)integerY;
-    vec2 distanceVector = vec2(dx, dy);
+    auto distanceVector = vec2(dx, dy);
 
     // Compute the dot-product.
     return dot(distanceVector, gradient);
@@ -64,7 +64,8 @@ vec2 randomGradient(int integerX, int integerY) {
     // No precomputed gradients mean this works for any number of grid coordinates
     const unsigned w = 8 * sizeof(unsigned);
     const unsigned s = w / 2;
-    unsigned a = integerX, b = integerY;
+    unsigned a = integerX;
+    unsigned b = integerY;
     a *= 3284157443;
 
     b ^= a << s | a >> w - s;
@@ -75,14 +76,13 @@ vec2 randomGradient(int integerX, int integerY) {
     float random = a * (3.14159265 / ~(~0u >> 1)); // in [0, 2*Pi]
 
     // Create the vector from the angle
-    vec2 v = vec2(sin(random), cos(random));
+    auto v = vec2(sin(random), cos(random));
 
     return v;
 }
 
 // Will interpolate between the first and second value, weight between 0 and 1.
-// NOTE: The function from the original tutorial might have been wrong (or I accidentally wrote it wrong...), 
-// so instead this interpolate implementation is used, which is based on the formula mentioned in this video
+// NOTE: This function is instead based on the formula from this video:
 // https://www.youtube.com/watch?v=ZsEnnB2wrbI
 float interpolate(float a, float b, float weight) {
     float t = weight * weight * weight * (weight * (weight * 6.0f - 15.0f) + 10.0f);
